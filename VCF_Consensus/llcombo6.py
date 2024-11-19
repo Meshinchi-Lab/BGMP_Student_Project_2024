ecccb6f4bb4ba898de955d1a767974e9bccb721d
Lena Allen <lenara@login2.talapas.uoregon.edu>
2024-11-19 14:48:12 -0800

    added X, Y , and M chroms

--- VCF_Consensus/llcombo6.py
+++ VCF_Consensus/llcombo6.py
@@ -161,7 +161,7 @@ for file in dir_list:
 
 #initalize
 svlist = ("INS", "DEL", "DUP", "BND", "INV")
-chrom_list = ("chr1", "chr2", "chr3","chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11","chr12", "chr13","chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21","chr22")
+chrom_list = ("chr1", "chr2", "chr3","chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11","chr12", "chr13","chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21","chr22", "chrX", "chrY", "chrM")
 chr_sv_combos =  list(itertools.product(chrom_list, svlist))
 
 #open all files by patient ID 
