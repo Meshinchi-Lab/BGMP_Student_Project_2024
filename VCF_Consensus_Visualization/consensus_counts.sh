c11e1ed98047f3b596b6ec4dacbf4a82a32e2ed1
Lena Allen <lenara@login3.talapas.uoregon.edu>
2024-12-05 19:12:24 -0800

    adding updated counts

--- VCF_Consensus_Visualization/consensus_counts.sh
+++ VCF_Consensus_Visualization/consensus_counts.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lenara@uoregon.edu
 
-/usr/bin/time -v ./consensus_counts.py -f /projects/bgmp/shared/groups/2024/aml/shared/llcombo8_output_files -o llcombo8_consensus_counts.tsv
+/usr/bin/time -v ./consensus_counts.py -f /projects/bgmp/shared/groups/2024/aml/shared/llcombo10_output_files -o llcombo10_consensus_counts.tsv
