a11931815dd856b756b23ec4d270480467ce09ad
Lauren Williams <lwil@uoregon.edu>
2024-12-19 19:24:46 -0800

    new script to find canonical fusions

--- Canonical_Fusions/canonfusions.py
+++ Canonical_Fusions/canonfusions.py
@@ -4,7 +4,7 @@ import os
 import re
 import itertools
 
-# ./canonfusions.py -input /projects/bgmp/shared/groups/2024/aml/shared/llcombo10_output_files -output /projects/bgmp/shared/groups/2024/aml/lwil/canon_fusions
+#./canonfusions.py -input /projects/bgmp/shared/groups/2024/aml/shared/llcombo10_output_files -output /projects/bgmp/shared/groups/2024/aml/lwil/canon_fusions -fusionsfile /projects/bgmp/shared/groups/2024/aml/lwil/BGMP_Student_Project_2024/Canonical_Fusions/canonical_paml_fusions.tsv
   
 #Functions
 def overlaps(span1, span2):
@@ -25,190 +25,70 @@ fusionsfile = args.fusionsfile
 #get all file names from folder and save as list
 dir_list = os.listdir(input)
 patient_list = []
-caller_list = []
 
 #pull patient IDs & caller names from file names: 
 for file in dir_list:
     x = re.split("\\.", file)
     if x[0] not in patient_list: 
         patient_list.append(x[0]) 
-    if x[1] not in caller_list: 
-        caller_list.append(x[1])
 
-#initalize
-svlist = ("INS", "DEL", "DUP", "BND", "INV")
-chrom_list = ("chr1", "chr2", "chr3","chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11","chr12", "chr13","chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21","chr22")
-chr_sv_combos =  list(itertools.product(svlist, chrom_list))
+found_fusions_dict = {}
+potential_fusions_list = []
+with open(f"{fusionsfile}", "r") as fusionsfile:
+    while True:
+        line = fusionsfile.readline()
+        line_as_list = line.strip().split("\t")
+        
+        if line_as_list == ['']:
+            break
 
-BND_dict: dict = {}
-SV1_BND_dict: dict = {}
-SV2_BND_dict: dict = {}
+        key = (line_as_list[0], line_as_list[1], line_as_list[2], line_as_list[3], line_as_list[4])
+        potential_fusions_list.append(key)
+        #     (gene, mate, chrom): start, end)
+        #ex: (CBFA2T3, GLIS2): (chr16, 88941267, 89043615)
+
 
 #open all files by patient ID 
 for patient in patient_list:
-    dictionary = {}
-    for caller in caller_list:
-        dictionary[caller] = {}
-    #POPULATING DICTIONARY -----------------------------------------------------------------------------------------------------
-    with open(f"{input}/{patient}.{caller_list[0]}.vcf", "r") as file1, open(f"{input}/{patient}.{caller_list[1]}.vcf", "r") as file2, open(f"{input}/{patient}.{caller_list[2]}.vcf", "r") as file3:
+    with open(f"{input}/{patient}.vcf", "r") as filein:
         while True:
-            line1 = file1.readline()
-            line_as_list1 = line1.strip().split("\t")
-            line2 = file2.readline()
-            line_as_list2 = line2.strip().split("\t")
-            line3 = file3.readline()
-            line_as_list3 = line3.strip().split("\t")
+            line = filein.readline()
+            line_as_list = line.strip().split("\t")
 
-            #caller 1-----------------------------------------------------------------------------------------------------------
-            if line_as_list1 == ['']:
+            if line_as_list == ['']:
                 break
-            
-            elif line_as_list1[0].startswith("#") != True:
-                line_as_list1[2] = caller_list[0]
-                #get start position
-                start_pos = int(line_as_list1[1])
-                #get chr
-                chrom = line_as_list1[0]
-                #get SVLEN and SVEND
-                info = line_as_list1[7]
-                infodict = build_infodict(info)
-                svtype = infodict['SVTYPE']
 
-                if svtype == "BND":
-                    #this is where abraham's breakend algorithm will go :)
-                    BND_tuple = BND_parser(line_as_list1)
-                    print(BND_tuple)
-                    SV1_BND_dict[BND_tuple] = line1
-                    continue
-                else:
+            if line_as_list[2] == "pbsv.BND" or line_as_list[2] == "sniffles.BND":
+                bnd_mate_pos = re.search(r"(?<=:)\d+", line_as_list[4])
+                bnd_mate_chrom = re.search(r"\w+(?=:)", line_as_list[4])
+                temp = (line_as_list[0], line_as_list[1], bnd_mate_chrom.group(), bnd_mate_pos.group())
+                #print(patient, value)
 
-                    svlen = infodict['SVLEN']
-                    if "," in svlen:
-                        svlen = svlen.split(",")
-                    else:
-                        svlen = abs(int(svlen))
-
-                    #get allelic depth
-                    #accounts for sniffles having their AD in the info line
-                    if "SUPPORT" in infodict:
-                        allelic_depth = infodict["SUPPORT"]
-
-                    #if caller is not sniffles
-                    else:
-                        sample_stats = line_as_list1[9]
-                        allelic_depth = sample_stats.split(":")[1]
-                        allelic_depth = allelic_depth.split(",")[1]
-
-                    #append to dictionary
-                    if type(svlen) != list:
-                        dictionary[caller_list[0]][(chrom, svtype, (start_pos, start_pos+svlen), allelic_depth)] = line1
-                    else:
-                        svlen = [int(x) for x in svlen]
-                        maxlen = max(svlen)
-                        dictionary[caller_list[0]][(chrom, svtype, (start_pos, start_pos+maxlen), allelic_depth)] = line1
-
-            #caller 2-----------------------------------------------------------------------------------------------------------
-            if line_as_list2 == ['']:
-                break 
-            
-            elif line_as_list2[0].startswith("#") != True:
-                line_as_list2[2] = caller_list[1]
-
-                #get start position
-                start_pos = int(line_as_list2[1])
-                #get chr
-                chrom = line_as_list2[0]
-                #get SVLEN and SVEND
-                info = line_as_list2[7]
-                infodict = build_infodict(info)
-                svtype = infodict['SVTYPE']
-
-                if svtype == "BND":
-                    #this is where abraham's breakend algorithm will go :)
-                    BND_tuple = BND_parser(line_as_list1)
-                    print(BND_tuple)
-                    SV1_BND_dict[BND_tuple] = line1
-                    continue
-                else:
-
-                    svlen = infodict['SVLEN']
-                    if "," in svlen:
-                        svlen = svlen.split(",")
-                    else:
-                        svlen = abs(int(svlen))
-                    end = infodict['END']
-
-                    #get allelic depth
-                    #accounts for sniffles having their AD in the info line
-                    if "SUPPORT" in infodict:
-                        allelic_depth = infodict["SUPPORT"]
-
-                    #if caller is not sniffles
-                    else:
-                        sample_stats = line_as_list2[9]
-                        allelic_depth = sample_stats.split(":")[1]
-                        allelic_depth = allelic_depth.split(",")[1]
-
-                    #append to dictionary
-                    if type(svlen) != list:
-                        dictionary[caller_list[1]][(chrom, svtype, (start_pos, start_pos+svlen), allelic_depth)] = line2
-                    else:
-                        svlen = [int(x) for x in svlen]
-                        maxlen = max(svlen)
-                        dictionary[caller_list[1]][(chrom, svtype, (start_pos, start_pos+maxlen), allelic_depth)] = line2
+                #check first gene
+                for fusion in potential_fusions_list:
+                    if fusion[2] == temp[0] and temp[1] >= fusion[3] and temp[1] <= fusion[4]:
+                        if fusion[1] == "X":
+                            print("gene 1 matches", patient, temp, fusion[0], fusion[1])
+                        else: 
+                            print("gene 1 matches, checking gene 2")
+                            matched_gene = fusion[1]
+                            #check matched gene
+                            for fusion2 in potential_fusions_list:
+                                if fusion2[0] == matched_gene and temp[2] == fusion2[2] and temp[3] >= fusion2[3] and temp[3] <= fusion2[4]:
+                                    print("gene 2 matches", patient, temp, fusion[0], fusion2[0])
+                    
+                    elif fusion[2] == temp[2] and temp[3] >= fusion[3] and temp[3] <= fusion[4]:
+                        if fusion[1] == "X":
+                            print("gene 1 matches", patient, temp, fusion[0], fusion[1])
+                        else: 
+                            print("gene 1 matches, checking gene 2")
+                            matched_gene = fusion[1]
+                            #check matched gene
+                            for fusion2 in potential_fusions_list:
+                                if fusion2[0] == matched_gene and temp[0] == fusion2[2] and temp[1] >= fusion2[3] and temp[1] <= fusion2[4]:
+                                    print("gene 2 matches", patient, temp, fusion[0], fusion2[0])
 
 
-            #caller 3-----------------------------------------------------------------------------------------------------------
-            if line_as_list3 == ['']:
-                break 
-            
-            elif line_as_list3[0].startswith("#") != True:
-                line_as_list3[2] = caller_list[2]
 
-                #get start position
-                start_pos = int(line_as_list3[1])
-                #get chr
-                chrom = line_as_list3[0]
-                #get SVLEN and SVEND
-                info = line_as_list3[7]
-                infodict = build_infodict(info)
-                svtype = infodict['SVTYPE']
 
-                if svtype == "BND":
-                    #this is where abraham's breakend algorithm will go :)
-                    BND_tuple = BND_parser(line_as_list1)
-                    print(BND_tuple)
-                    SV1_BND_dict[BND_tuple] = line1
-                    continue
-                else:
-
-                    svlen = infodict['SVLEN']
-                    if "," in svlen:
-                        svlen = svlen.split(",")
-                    else:
-                        svlen = abs(int(svlen))
-                    end = infodict['END']
-
-                    #get allelic depth
-                    #accounts for sniffles having their AD in the info line
-                    if "SUPPORT" in infodict:
-                        allelic_depth = infodict["SUPPORT"]
-
-                    #if caller is not sniffles
-                    else:
-                        sample_stats = line_as_list3[9]
-                        allelic_depth = sample_stats.split(":")[1]
-                        allelic_depth = allelic_depth.split(",")[1]
-
-                    #append to dictionary
-                    if type(svlen) != list:
-                        dictionary[caller_list[2]][(chrom, svtype, (start_pos, start_pos+svlen), allelic_depth)] = line3
-                    else:
-                        svlen = [int(x) for x in svlen]
-                        maxlen = max(svlen)
-                        dictionary[caller_list[2]][(chrom, svtype, (start_pos, start_pos+maxlen), allelic_depth)] = line3
-
-    with open(f"{output}/{patient}_CONSENSUS.vcf", "w") as fo:
-        for callers in caller_list:
-            for key in dictionary[caller]:
-                fo.write(dictionary[caller][key])
+#with open(f"{output}/{patient}_CONSENSUS.vcf", "w") as fileout:
