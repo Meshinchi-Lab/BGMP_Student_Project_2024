8cbcea5310c7e90885fa8fae0ead49ad56c70af5
Abraham Solomon <asol@login2.talapas.uoregon.edu>
2025-03-05 03:30:33 -0800

    Removed redundant VCF_consensus_visualizations

--- VCF_Consensus_Visualization/Full_Dataset_Consensus_Visualizations.md
+++
@@ -1,7 +0,0 @@
-# Full Dataset Consensus Visualizations
-
-![alt text](fusionplot.png)
-
-![alt text](Absolute_BND_Freq_By_Chrom.png)
-
-![alt text](BND_Freq_By_Chrom_Normalized.png)
--- VCF_Consensus_Visualization/consensus_counts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --partition=bgmp
-#SBATCH --account=bgmp
-#SBATCH --mem=100G
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=lenara@uoregon.edu
-
-/usr/bin/time -v ./consensus_counts.py -f /projects/bgmp/shared/groups/2024/aml/shared/llcombo10_output_files -o llcombo10_consensus_counts.tsv
